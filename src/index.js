38a348e48fe333931e6ed3fb4e80e4ef136956e5
github username <github email address>
2020-01-07 10:51:57 -0600

    user can submit text and will be displayed (needs squelch)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+let li = document.createElement('li')
+let ulInput = document.getElementById('tasks')
+let actualInput = document.getElementById('new-task-description').value
+
+
+function addText (str) {
+  li.innerHTML = str
+  ulInput.appendChild(li)
+  // ulInput.innerText = str
+
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addText("havilah is FLAK")
 });
 
 
-let contentInput = document.getElementById('tasks')
+
+
