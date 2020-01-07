296139d7cf970784215b1f421bf0d4219a9b49bc
github username <github email address>
2020-01-07 11:12:14 -0600

    can add multiple lines of text

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-let li = document.createElement('li')
-let ulInput = document.getElementById('tasks')
-let actualInput = document.getElementById('new-task-description').value
+// let li = document.createElement('li')
+// let ulInput = document.getElementById('tasks')
+// let actualInput = document.getElementById('new-task-description').value
 
 
-function addText (str) {
-  li.innerHTML = str
-  ulInput.appendChild(li)
-  // ulInput.innerText = str
-
-}
+// function addText (str) {
+//   li.innerHTML = str
+//   ulInput.appendChild(li)
+// }
 
 
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById('create-task-form').addEventListener("submit", function(event) {
     event.preventDefault();
-  });
 
-  addText("havilah is FLAK")
+    function addText (str) {
+      li.innerHTML = str
+      ulInput.appendChild(li)
+    }
+    let li = document.createElement('li')
+    let ulInput = document.getElementById('tasks')
+    let actualInput = document.getElementById('new-task-description').value;
+    addText(actualInput);
+  });
 });
 
 
