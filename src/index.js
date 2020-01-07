5b4c7fd24b5c6112c552ee6ccbd0f013c56dd5c4
github username <github email address>
2020-01-07 12:29:53 -0600

    working

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById('create-task-form').addEventListener("submit", function(event) {
     event.preventDefault();
-
     function addText (str) {
       li.innerHTML = str
       ulInput.appendChild(li)
